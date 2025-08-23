1e117b714449633821d4946872ec8e7cf2f1e194
Vinayaka Swamy H M <86816121+VinZCodz@users.noreply.github.com>
2025-08-23 15:58:48 +0530

    basic setup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,23 @@
-// import { Example } from "./example";
+import OpenAI from "openai";
+import dotenv from "dotenv";
 
-console.log("Vinay");
-console.log("weatherAI");
+dotenv.config({ path: '.env.local', override: true });// Add your own .env.local
 
+const openai=new OpenAI({
+    apiKey: process.env.GOOGLE_API_KEY,
+    baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/"
+});
+
+const response=openai.chat.completions.create({
+    model: "gemini-2.0-flash-lite",
+    reasoning_effort: "low",
+    messages:[
+        { role: "system", content: "You are a helpful assistant." },
+        {
+            role: "user",
+            content: "Explain to me how AI works",
+        },
+    ]
+});
+
+console.log(response.choices[0].message);   
