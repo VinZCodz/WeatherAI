1d848e552b4143549188da1297fd9e9293e1f89b
Vinayaka Swamy H M <86816121+VinZCodz@users.noreply.github.com>
2025-08-23 12:29:23 +0000

    basic integration and running proj

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,19 @@ import dotenv from "dotenv";
 dotenv.config({ path: '.env.local', override: true });// Add your own .env.local
 
 const openai=new OpenAI({
-    apiKey: process.env.GOOGLE_API_KEY,
+    apiKey: process.env.GOOGLE_API_KEY as string,
     baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/"
 });
 
-const response=openai.chat.completions.create({
+const response = await openai.chat.completions.create({
     model: "gemini-2.0-flash-lite",
-    reasoning_effort: "low",
-    messages:[
+    messages: [
         { role: "system", content: "You are a helpful assistant." },
         {
             role: "user",
             content: "Explain to me how AI works",
         },
-    ]
+    ],
 });
 
-console.log(response.choices[0].message);   
+console.log(response.choices[0]?.message.content); 
